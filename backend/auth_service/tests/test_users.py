73ad37b18dc304818631bd017c17b95d3438d64e
nahid Qaderi <nahid.ghaderee@gmail.com>
2026-05-22 21:12:27 -0700

    Fix CI Python import paths

--- backend/auth_service/tests/test_users.py
+++ backend/auth_service/tests/test_users.py
@@ -1,7 +1,6 @@
 from fastapi.testclient import TestClient
 import pytest
 from app.main import app
-from app.core.security import get_current_identity
 from app.database.db import get_db, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import  sessionmaker
